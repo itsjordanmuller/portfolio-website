f06bcd21189c55502e80a9eab7d63bcf0bd32f4d
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-30 21:38:21 -0800

    Update content collection schema, add resources

--- src/content/config.ts
+++ src/content/config.ts
@@ -34,7 +34,26 @@ const projectsCollection = defineCollection({
 	}),
 });
 
+const resourcesCollection = defineCollection({
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		publishDate: z.date(),
+		description: z.string(),
+		author: z.string(),
+		images: z.array(
+			z.object({
+				url: z.string(),
+				alt: z.string(),
+			}),
+		),
+		tags: z.array(z.string()),
+		type: z.string(),
+	}),
+});
+
 export const collections = {
 	posts: postsCollection,
 	projects: projectsCollection,
+	resources: resourcesCollection,
 };
