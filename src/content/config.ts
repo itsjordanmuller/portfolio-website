73436b3d182a1505dfed399c46edeb60cb555af5
itsjordanmuller <itsjordanmuller@gmail.com>
2023-12-17 21:57:03 -0800

    Update Portfolio Project components/layouts Use Astro Image component instead of standard img element

--- src/content/config.ts
+++ src/content/config.ts
@@ -18,22 +18,21 @@ const postsCollection = defineCollection({
 
 const projectsCollection = defineCollection({
 	type: "content",
-	schema: z.object({
-		title: z.string(),
-		publishDate: z.date(),
-		description: z.string(),
-		author: z.string(),
-		images: z.array(
-			z.object({
-				url: z.string(),
-				alt: z.string(),
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			publishDate: z.date(),
+			description: z.string(),
+			author: z.string(),
+			cover: image().refine((img) => img.width >= 100, {
+				message: "100px",
 			}),
-		),
-		repo: z.string(),
-		demo: z.string(),
-		importance: z.number(),
-		tags: z.array(z.string()),
-	}),
+			coverAlt: z.string(),
+			repo: z.string(),
+			demo: z.string(),
+			importance: z.number(),
+			tags: z.array(z.string()),
+		}),
 });
 
 const resourcesCollection = defineCollection({
