8318e6d578e6410b5bb1bcb2ae921e4cb6e241ba
itsjordanmuller <itsjordanmuller@gmail.com>
2023-12-17 21:16:36 -0800

    Update layouts & components related to Posts Change the way that images are loaded to use Astro Image method

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,19 +2,18 @@ import { z, defineCollection } from "astro:content";
 
 const postsCollection = defineCollection({
 	type: "content",
-	schema: z.object({
-		title: z.string(),
-		publishDate: z.date(),
-		description: z.string(),
-		author: z.string(),
-		images: z.array(
-			z.object({
-				url: z.string(),
-				alt: z.string(),
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			publishDate: z.date(),
+			description: z.string(),
+			author: z.string(),
+			cover: image().refine((img) => img.width >= 100, {
+				message: "100px",
 			}),
-		),
-		tags: z.array(z.string()),
-	}),
+			coverAlt: z.string(),
+			tags: z.array(z.string()),
+		}),
 });
 
 const projectsCollection = defineCollection({
